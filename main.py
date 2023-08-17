63e87df8c6c0c5c37d6a3b3a8c78d38359da98ee
Danylo <danylo.solohub.te.2019@lpnu.ua>
2023-08-17 20:30:43 +0300

    added main.py with commented code

---
+++ main.py
@@ -0,0 +1,16 @@
+# example_1 = [{'a': 1}, {'b': 2}, {'c': 3}]
+# for item in example_1:
+#     for key, value in item.items():
+#         print(key, value)
+
+# example_2 = {'a': [1], 'b': [1, 2], 'c': [1, 2, 3]}
+# for key, value in example_2.items():
+#     print(key, value)
+
+# example_3 = {'a': {'b': {'c': 1}}}
+# for key, value in example_3.items():
+#     print(key, value)
+#     for key_2, value_2 in value.items():
+#         print(key_2, value_2)
+#         for key_3, value_3 in value_2.items():
+#             print(key_3, value_3)
